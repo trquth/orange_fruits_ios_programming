12f2916a1805e9c756c00703141b889e4abc8142
thientran <thien.tran@saigontechnology.vn>
2017-12-13 21:59:23 +0700

    Initial project

---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  orange_fruits_ios_programming
+//
+//  Created by Thien Tran on 12/13/17.
+//  Copyright © 2017 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
