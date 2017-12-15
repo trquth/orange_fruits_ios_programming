da0db4a097fb9cc02d8d5429b1375812b2f9ccfa
trquth <tranquthien@gmail.com>
2017-12-15 15:25:10 +0700

    Practice ScrollView

--- orange_fruits_ios_programming/orange_fruits_ios_programming/AppDelegate.swift
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/AppDelegate.swift
@@ -17,8 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: TextViewController())
-        window?.makeKeyAndVisible()
+        self.window?.rootViewController = UINavigationController(rootViewController: NormalViewController())
+        self.window?.makeKeyAndVisible()
         
         return true
     }
