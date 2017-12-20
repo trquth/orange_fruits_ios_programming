5456dda56505ea0ba89764d0af358829a82a94d3
trquth <tranquthien@gmail.com>
2017-12-20 16:39:51 +0700

    Practice call API

---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/Networking/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  orange_fruits_ios_programming
+//
+//  Created by mac on 12/20/17.
+//  Copyright © 2017 Thien Tran. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let twitterUrl : String = "https://api.twitter.com/1.1/"
+}
