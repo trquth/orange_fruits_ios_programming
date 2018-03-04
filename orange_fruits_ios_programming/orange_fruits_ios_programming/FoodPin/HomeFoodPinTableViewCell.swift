079bec29583fefcf149d440cf7cfe0001ab2d990
thientran <thien.tran@saigontechnology.vn>
2018-03-04 20:21:50 +0700

    Basic table view and custom cell

---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/FoodPin/HomeFoodPinTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeFoodPinTableViewCell.swift
+//  orange_fruits_ios_programming
+//
+//  Created by Thien Tran on 3/4/18.
+//  Copyright © 2018 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+
+
+class HomeFoodPinTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel : UILabel!
+    @IBOutlet var locationLabel : UILabel!
+    @IBOutlet var typeLabel : UILabel!
+    @IBOutlet var thumnailImageView : UIImageView!
+    
+    
+}
---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/FoodPin/HomeFoodPinViewController.swift
@@ -0,0 +1,52 @@
+//
+//  HomeFoodPinViewController.swift
+//  orange_fruits_ios_programming
+//
+//  Created by Thien Tran on 3/4/18.
+//  Copyright © 2018 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+
+class HomeFoodPinViewController : UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    private let cellIdentifier = "Cell"
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! HomeFoodPinTableViewCell
+        
+        let index = indexPath.row
+        cell.nameLabel?.text = restaurantNames[index]
+        cell.locationLabel?.text = restaurantLocations[index]
+        cell.typeLabel?.text = restaurantTypes[index]
+        cell.thumnailImageView?.image = UIImage(named : restaurantImages[index] )
+        
+        return cell
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    
+}
