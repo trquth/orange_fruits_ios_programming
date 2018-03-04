e34c33d4c4e91e53a85014c2fa3fb610f76656cf
thientran <thien.tran@saigontechnology.vn>
2018-03-04 09:43:13 +0700

    Use INTERFACE BUILDER to create UI application Use AUTO LAYOUT and STACK VIEW to set constraint for all elements Use SIZE CLASS to configure attributes of element with another screens

---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  orange_fruits_ios_programming
+//
+//  Created by Thien Tran on 3/4/18.
+//  Copyright © 2018 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
