2bd2188560e82362790af4c46ffcc587c47e5e52
thientran <thien.tran@saigontechnology.vn>
2018-03-14 21:47:49 +0700

    1.Create Login Screen by Interface Builder 2.User addGestureRecognizer to add a private action with button 3.Break new branch to research navigate to another screen Link : https://badootech.badoo.com/screen-navigation-in-ios-dd99b09228b2 Source : https://github.com/BohdanOrlov/NavigationExamples

---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/Controllers/SignIn/SignInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignInViewController.swift
+//  orange_fruits_ios_programming
+//
+//  Created by Thien Tran on 3/14/18.
+//  Copyright © 2018 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(signInTap))
+        self.signInButton.addGestureRecognizer(tapGesture)
+    }
+
+    //Prive methods
+    
+   @objc private func signInTap(){
+        print("PRESS HERE")
+    }
+}
