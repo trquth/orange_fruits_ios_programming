12de8b5e5f6949996494a595d84888344270f628
thientran <thien.tran@saigontechnology.vn>
2018-07-16 06:44:42 +0700

    Start new project

---
+++ orange_fruits_ios_programming/orange_fruits_ios_programming/TabBar/TabBarNavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  TabNavigationController.swift
+//  orange_fruits_ios_programming
+//
+//  Created by Thien Tran on 7/14/18.
+//  Copyright © 2018 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class  CustomTabBarNavigationController: UITabBarController {
+    
+}
